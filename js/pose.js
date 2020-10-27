dbede8d6d595f871e7492b2e9364177d9819c69d
lapiscine <stephane.cursan@lapiscine.pro>
2020-10-27 15:08:12 +0100

    Update pose.js
    Class pose 2

--- js/pose.js
+++ js/pose.js
@@ -13,13 +13,29 @@ class Pose extends HTMLElement{
         this.initListeners();
     }
 
+    initListeners(){
+        var event = new CustomEvent("poseClicked", {
+            detail: "pose" + this.id 
+        });
+        this.dispatchEvent(event);
+    }
+
     createPose(){
         this.setAttribute("data-id", this.id);
         this.setAttribute("class", "poseBtn");
+        this.setAttribute("disabled", this.disabled);
         var poseImg = document.createElement('img');
         poseImg.setAttribute("class", "poseImg");
         poseImg.setAttribute("url", "assets/pictures/" + this.urlImage + ".jpg");
+        var poseSound = document.createElement("audio");
+        poseSound.setAttribute("class", "poseSound");
+        poseSound.setAttribute("src", "assets/sounds/" + this.urlSound + ".mp3");
+        this.appendChild(poseImg);
+        this.appendChild(poseSound);
+    }
 
+    playsSound(){
+        
     }
 
 }
