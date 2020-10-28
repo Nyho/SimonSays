d8da8043dce5a35d84a58f6f8f1101dab7731336
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-10-28 11:26:57 +0100

    wip in progress

--- js/gameFunction.js
+++ js/gameFunction.js
@@ -1,41 +1,51 @@
-const image = [image1, image2, image3, image4, image5, image6]
-const son = [son1, son2, son3, son4, son5, son6]
+var poseParameters = [
+    { poseName : 'test1', soundName : 'test1', imageName : 'test1'},
+    { poseName : 'test2', soundName : 'test1', imageName : 'test2'},
+    { poseName : 'test3', soundName : 'test1', imageName : 'test3'},
+    { poseName : 'test4', soundName : 'test1', imageName : 'test4'},
+    { poseName : 'test5', soundName : 'test1', imageName : 'test5'},
+    { poseName : 'test6', soundName : 'test1', imageName : 'test6'}
+]
 
 /**
  * génère des poses dans le document html
  */
+
 function generateStaticPoses() {
-    for (var i = 1; i < 6; i++) {
-        let pose = new Pose(i, image[i], son[i]);
-        document.body.appendChild(pose);
+    var tabLenght = poseParameters.length
+    for (var i = 0; i < tabLenght; i++) {
+        var poseData = poseParameters[i];
+        console.log(i);
+        let pose = new Pose(poseData.poseName, poseData.soundName, poseData.imageName);
+        document.getElementById("mainContainer").appendChild(pose)
     }
 }
 
 /**
  * génère un tableau de pose
  */
-function generatePoses() {
-    var poses = [];
-    for (var i = 1; i < 6; i++) {
-        poses.push(new Pose(i, image[i], son[i]));
-    }
-    return poses;
-}
+// function generatePoses() {
+//     var poses = [];
+//     for (var i = 1; i < 6; i++) {
+//         poses.push(new Pose(i, image[i], son[i]));
+//     }
+//     return poses;
+// }
 
 /**
  * randomize un tableau de pose
  */
-function getRandomPoses(poses) {
-    return poses[Math.floor(Math.random() * poses.length)];
-}
+// function getRandomPoses(poses) {
+//     return poses[Math.floor(Math.random() * poses.length)];
+// }
 
 
 /**
  * Récupère une pose aléatoire et la stock dans un tableau sequence
  */
-const sequence = [
-    getRandomPoses(generatePoses())
-];
+// const sequence = [
+//     getRandomPoses(generatePoses())
+// ];
 
 // Pour chaque séquence, appeler une promesse qui affiche une pose, qui attend qu'elle disparaisse pour afficher
 // la pose suivante s'il n'y a pas de suivante c'est au joueur de jouer et appeler l'eventListener
--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
-function testStart() {
-    var pose = new Pose("highKick", "test1", "test1");
-    var poseBtn = document.getElementById("mainContainer").appendChild(pose);
-    poseBtn.addEventListener("poseClicked",function (e) {
-        console.log(this);
-    });
-}
-testStart();
+
+generateStaticPoses();
