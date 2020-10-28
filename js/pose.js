e83fe84f3f30395d4cb52f51b2f4afb0215c8965
Hugo Nacry <hugo.nacry@gmail.com>
2020-10-28 10:09:25 +0100

    class pose 3

--- js/pose.js
+++ js/pose.js
@@ -1,49 +1,67 @@
 class Pose extends HTMLElement {
     poseName;
-    urlImage;
-    disabled = true;
+    imageName;
     audio;
+    disabled = false;
 
-    constructor(pose, soundUrl) {
+    constructor(poseName, soundName, imageName) {
         super();
-        this.poseName = pose;
-        this.audio = this.createAudioEl(soundUrl);
+        this.poseName = poseName;
+        this.imageName = imageName;
+        this.audio = this.createAudioEl(soundName);
+        this.createPose(imageName);
         this.initListeners();
-        this.createPose();
-    }
-
-    createAudioEl(soundUrl){
-        const audio = new Audio();
-        audio.setAttribute('style','display: none;');
-        this.appendChild(audio);
-        audio.src = 'assets/sounds/' + soundUrl + '.mp3';
-        return audio;
     }
 
     initListeners(){
         this.onclick = function () {
-            this.playsSound().then(function () {
+            console.log(!this.disabled);
+            if(!this.disabled){
+                this.playsSound().then(function () {
 
-            });
-            var event = new CustomEvent("poseClicked", {
-                detail: "pose" + this.id
-            });
-            this.dispatchEvent(event);
+                });
+                var event = new CustomEvent("poseClicked", {
+                    detail: this.poseName
+                });
+                this.dispatchEvent(event);
+            }
         }
     }
 
-    createPose(){
-        /*this.setAttribute("data-id", this.id);*/
-        this.setAttribute("class", "poseBtn");
-        /*this.setAttribute("disabled", this.disabled.toString());*/
-        var poseImg = document.createElement("img");
-        poseImg.setAttribute("class", "poseImg");
-        poseImg.setAttribute("src", "assets/pictures/test1.jpg");
-        this.appendChild(poseImg);
+    createAudioEl(soundName){
+        const audio = new Audio();
+        audio.setAttribute('style','display: none;');
+        this.appendChild(audio);
+        audio.src = 'assets/sounds/' + soundName + '.mp3';
+        audio.preload;
+        return audio;
+    }
+
+    isDisabled(status){
+        this.disabled = status;
     }
 
     playsSound(){
-        this.audio.play();
+        var me = this;
+        return new Promise(function (resolve, reject) {
+            me.audio.play();
+            me.audio.onended = function () {
+                console.log("Tout s'est correctement déroulé.");
+                resolve();
+            };
+            me.audio.onerror = function (err) {
+                reject(err)
+            }
+        })
+    }
+
+    createPose(imageName){
+        this.setAttribute("data-name", this.poseName);
+        this.setAttribute("class", "poseBtn");
+        var poseImg = document.createElement("img");
+        poseImg.setAttribute("class", "poseImg");
+        poseImg.setAttribute("src", "assets/pictures/" + imageName + ".jpg");
+        this.appendChild(poseImg);
     }
 
 }
